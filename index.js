90ae41f44f4c1b138f3a263840904725fdda3546
Ralf S. Engelschall <rse@engelschall.com>
2013-12-30 23:07:34 +0100

    hide arrows at the borders

--- index.js
+++ index.js
@@ -22,6 +22,14 @@
                 draggable: true,
                 hide: false,
                 snapOnRelease: true
+            },
+            onSlideChangeEnd: function () {
+                $(".icons .fa-arrow-circle-left").show();
+                $(".icons .fa-arrow-circle-right").show();
+                if (swiper.activeIndex === 0)
+                    $(".icons .fa-arrow-circle-left").hide();
+                else if (swiper.activeIndex === 3)
+                    $(".icons .fa-arrow-circle-right").hide();
             }
         })
         $(".icons .fa-arrow-circle-left").click(function (ev) {
