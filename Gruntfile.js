f3c44ded2087c234b1e406c77cb57b435641b46c
Ralf S. Engelschall <rse@engelschall.com>
2017-12-02 21:18:11 +0100

    switch from bower to NPM and upgrade to latest version of dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,66 +3,48 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-bower-install-simple");
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        "bower-install-simple": {
-            "main": {
-                options: {
-                    color:       true,
-                    production:  true,
-                    directory:   "bower_components"
-                }
-            }
-        },
         copy: {
             "lodash": {
-                src: [ "bower_components/lodash/dist/lodash.js" ],
+                src: [ "node_modules/lodash/lodash.min.js" ],
                 dest: "lib/lodash/lodash.js"
             },
             "nunjucks": {
-                src: [ "bower_components/nunjucks/browser/nunjucks.js" ],
+                src: [ "node_modules/nunjucks/browser/nunjucks.js" ],
                 dest: "lib/nunjucks/nunjucks.js"
             },
             "jquery": {
-                src: [ "bower_components/jquery/dist/jquery.js" ],
+                src: [ "node_modules/jquery/dist/jquery.js" ],
                 dest: "lib/jquery/jquery.js"
             },
             "jquery-markup": {
-                src: [ "bower_components/jquery-markup/jquery.markup.js" ],
+                src: [ "node_modules/jquery-markup/jquery.markup.js" ],
                 dest: "lib/jquery-markup/jquery.markup.js"
             },
             "director": {
-                src: [ "bower_components/director/build/director.js" ],
+                src: [ "node_modules/director/build/director.js" ],
                 dest: "lib/director/director.js"
             },
             "swiper": {
-                src: [ "bower_components/swiper/dist/idangerous.swiper.min.js" ],
-                dest: "lib/swiper/idangerous.swiper.js"
+                src: [ "node_modules/swiper/dist/js/swiper.js" ],
+                dest: "lib/swiper/swiper.js"
             },
             "swiper-css": {
-                src: [ "bower_components/swiper/dist/idangerous.swiper.css" ],
-                dest: "lib/swiper/idangerous.swiper.css"
+                src: [ "node_modules/swiper/dist/css/swiper.css" ],
+                dest: "lib/swiper/swiper.css"
             },
-            "swiper-plugins": {
-                files: {
-                    "lib/swiper/idangerous.swiper.scrollbar.js":  "bower_components/swiper-scrollbar/dist/idangerous.swiper.scrollbar.js",
-                    "lib/swiper/idangerous.swiper.scrollbar.css": "bower_components/swiper-scrollbar/dist/idangerous.swiper.scrollbar.css",
-                    "lib/swiper/idangerous.swiper.hashnav.js":    "bower_components/swiper-hash-navigation/dist/idangerous.swiper.hashnav.js",
-                    "lib/swiper/idangerous.swiper.progress.css":  "bower_components/swiper-smooth-progress/dist/idangerous.swiper.progress.js"
-                }
+            "syntax": {
+                src: [ "node_modules/syntax/lib/syntax.browser.js" ],
+                dest: "lib/syntax/syntax.js"
             },
-            "highlight": {
-                src: [ "bower_components/highlightjs/highlight.pack.js" ],
-                dest: "lib/highlight/highlight.js"
-            },
-            "sanitize": {
-                src: [ "bower_components/sanitize.css/dist/sanitize.css" ],
-                dest: "lib/sanitize/sanitize.css"
+            "normalize.css": {
+                src: [ "node_modules/normalize.css/normalize.css" ],
+                dest: "lib/normalize/normalize.css"
             },
             "font-awesome-css": {
-                src: [ "bower_components/font-awesome/css/font-awesome.css" ],
+                src: [ "node_modules/font-awesome/css/font-awesome.css" ],
                 dest: "lib/font-awesome/font-awesome.css",
                 options: {
                     process: function (content, srcpath) {
@@ -72,34 +54,34 @@ module.exports = function (grunt) {
             },
             "font-awesome-fonts": {
                 files: [{
-                    expand: true, flatten: false, cwd: "bower_components/font-awesome/fonts",
+                    expand: true, flatten: false, cwd: "node_modules/font-awesome/fonts",
                     src: "fontawesome-webfont.*",
                     dest: "lib/font-awesome/"
                 }]
             },
             "gridless": {
-                src: [ "bower_components/gridless/predef/eg12.css" ],
+                src: [ "node_modules/gridless/predef/eg12.css" ],
                 dest: "lib/gridless/eg12.css"
             },
             "typopro": {
                 files: [
-                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-OpenSans/**", dest: "lib/typopro/" },
-                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-DejaVu/**", dest: "lib/typopro/" },
-                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-YanoneKaffeesatz/**", dest: "lib/typopro/" },
-                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-Journal/**", dest: "lib/typopro/" }
                 ]
             }
         },
         clean: {
             clean:     [ "lib" ],
-            distclean: [ "node_modules", "bower_components" ]
+            distclean: [ "node_modules", "node_modules" ]
         }
     });
 
-    grunt.registerTask("default", [ "bower-install-simple", "copy" ]);
+    grunt.registerTask("default", [ "copy" ]);
 };
 
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 
 (function ($) {
     $(document).ready(function () {
-        $("pre.highlight").each(function () {
-            hljs.highlightBlock(this, true, false);
-        });
-        var swiper = $(".swiper-container").swiper({
+        $("*[data-syntax]").each(function () {
+            var language = $(this).data("syntax")
+            var syntax = new Syntax({
+                language: language,
+                cssPrefix: "syntax-"
+            })
+            syntax.config({})
+            var text = $(this).text()
+            text = text
+                .replace(/^(?:[ \t]*\r?\n)+/, "")
+                .replace(/([ \t]*\r?\n)(?:[ \t]*\r?\n)*[ \t]*$/, "$1")
+            syntax.richtext(text)
+            var html = syntax.html()
+            $(this).html(html)
+            $(".syntax-anchor", this).each(function () {
+                var m = $(this).attr("class").match(/syntax-anchor-(\S+)/)
+                var num = m[1]
+                $(this).addClass("cn-" + num + "-i")
+            })
+            $(this).addClass("syntax")
+        })
+        var swiper = new Swiper(".swiper-container", {
             freeMode: false,
             freeModeFluid: false,
             mode: "horizontal",
             loop: false,
             autoResize: true,
-            keyboardControl: true,
-            mousewheelControl: false,
+            keyboard: true,
+            mousewheel: false,
             pagination: ".pagination",
             paginationClickable: true,
             createPagination: true,
@@ -34,11 +52,11 @@
         })
         $(".icons .fa-arrow-circle-left").click(function (ev) {
             ev.preventDefault()
-            swiper.swipePrev()
+            swiper.slidePrev()
         })
         $(".icons .fa-arrow-circle-right").click(function (ev) {
             ev.preventDefault()
-            swiper.swipeNext()
+            swiper.slideNext()
         })
     })
 })(jQuery)
